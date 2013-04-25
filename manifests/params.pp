bdbea821b7520cfeb671b48d54bd75d0836300fd
William Leese <william.leese@meltwater.com>
2013-04-25 14:50:25 +0900

    x

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,10 +3,7 @@ class tomcat::params {
   $default_source_release = "6.0.26"
   $default_source_release_v55 = "5.5.27"
 
-  $instance_basedir = $tomcat_instance_basedir ? {
-    ""      => "/srv/tomcat",
-    default => $tomcat_instance_basedir,
-  }
+  $instance_basedir = "/srv/tomcat"
 
   if $tomcat_mirror {
     $mirror = $tomcat_mirror
